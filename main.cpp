afbcea7d1e85e62e3749c4795d85a798bd6f0323
BDZNH <pxezhsp@live.com>
2018-01-01 21:18:33 +0800

    change the method of ifstream

--- main.cpp
+++ main.cpp
@@ -18,58 +18,73 @@ void gogogo(char *name,char *command)
         cin>>num;
     }
     cout<<"Downloading : No."<<num++<<" "<<name<<endl;
-    cout<<command<<endl;
-//    system(command);
+//    cout<<command<<endl;
+    system(command);
 }
 int main ()
 {
-    freopen("download_link.txt","r",stdin);
-    char url[1000]={0},cc=0,name[1000]={0},command[10000]={"aria2c -s 10 -x 10 \""},dd=0,bb[4]={0};
+//    freopen("download_link.txt","r",stdin);
+    ifstream InFile("download_link.txt");
+    if(!InFile.is_open())
+    {
+        cout<<"Can't open the download_link.txt. No such a file"<<endl;
+        system("pause");
+        return 0;
+    }
+    char url[1000]={0},cc=0,name[1000]={0},command[10000]={"aria2c -s 10 -x 10 \""},dd=0,bb[5]={0},temp[1000]={0};
     int Len=strlen(command);
     bool ft=true;
-    int i=0,j=0,len,haha=1;
-    while(scanf("%c",&dd)!=EOF)
+    int i=0,j=0,len,haha=1,len2,k;
+//    while(scanf("%c",&dd)!=EOF)
+    while(InFile.getline(temp,999))
     {
-        if(cc=='\n'&&dd=='\n')
-            continue;
-        cc=dd;
-        if(cc=='\n')
+        len2=strlen(temp);
+        temp[len2]='\n';
+        temp[len2+1]=0;
+        for(k=0;k<=len2;k++)
         {
-            strcat(command,url);
-            strcat(command,"\" -o \"src/");
-            sprintf(bb,"%02d ",haha++);
-            strcat(command,bb);
-            strcat(command,name);
-            len=strlen(url);
-            if(url[len-1]=='v')
-                strcat(command,".flv\"\n");
-            if(url[len-1]=='4')
-                strcat(command,".mp4\"\n");
-//            printf("%d     %s",count++,command);
-//            system(command);
+            dd=temp[k];
+            if(cc=='\n'&&dd=='\n')
+                continue;
+            cc=dd;
+            if(cc=='\n')
+            {
+                strcat(command,url);
+                strcat(command,"\" -o \"src/");
+                sprintf(bb,"%03d ",haha++);
+                strcat(command,bb);
+                strcat(command,name);
+                len=strlen(url);
+                if(url[len-1]=='v')
+                    strcat(command,".flv\"\n");
+                if(url[len-1]=='4')
+                    strcat(command,".mp4\"\n");
+    //            printf("%d     %s",count++,command);
+    //            system(command);
 
-            gogogo(name,command);
-            command[Len]=0;
-            memset(url,0,sizeof(char)*1000);
-            memset(name,0,sizeof(char)*1000);
-            memset(command+20,0,sizeof(char)*1000);
-            i=0;
-            j=0;
-            ft=true;
-            continue;
-        }
-        if(cc=='+'||cc=='|')
-        {
-            ft=false;
-            continue;
-        }
-        if(ft)
-        {
-            url[i++]=cc;
-        }
-        else
-        {
-            name[j++]=cc;
+                gogogo(name,command);
+                command[Len]=0;
+                memset(url,0,sizeof(char)*1000);
+                memset(name,0,sizeof(char)*1000);
+                memset(command+20,0,sizeof(char)*1000);
+                i=0;
+                j=0;
+                ft=true;
+                continue;
+            }
+            if(cc=='+'||cc=='|')
+            {
+                ft=false;
+                continue;
+            }
+            if(ft)
+            {
+                url[i++]=cc;
+            }
+            else
+            {
+                name[j++]=cc;
+            }
         }
     }
     if(cc!='\n')
